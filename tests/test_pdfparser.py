666130575bda05deee80ff804816ce7232b41134
David Huggins-Daines <dhd@ecolingui.ca>
2024-09-17 20:47:45 -0400

    fix(test): BufferedReader not necessary

--- tests/test_pdfparser.py
+++ tests/test_pdfparser.py
@@ -2,7 +2,7 @@
 Test the PDF parser
 """
 
-from io import BufferedReader, BytesIO
+from io import BytesIO
 
 import pytest
 
@@ -45,7 +45,7 @@ EXPECTED = [
 
 def test_nextline():
     """Verify that we replicate the old nextline method."""
-    parser = PSBaseParser(BufferedReader(BytesIO(TESTDATA)))
+    parser = PSBaseParser(BytesIO(TESTDATA))
     lines = []
     while True:
         try:
@@ -58,7 +58,7 @@ def test_nextline():
 
 def test_revreadlines():
     """Verify that we replicate the old revreadlines method."""
-    parser = PSBaseParser(BufferedReader(BytesIO(TESTDATA)))
+    parser = PSBaseParser(BytesIO(TESTDATA))
     lines = list(parser.revreadlines())
     assert lines == list(reversed([line for pos, line in EXPECTED]))
 
@@ -92,13 +92,15 @@ SIMPLETOK = [
 
 
 def test_new_parser():
-    parser = PSBaseParser(BufferedReader(BytesIO(SIMPLE1 * 10000)))
+    # Do a lot of them to make sure buffering works correctly
+    parser = PSBaseParser(BytesIO(SIMPLE1 * 100))
     tokens = [tok for pos, tok in list(parser)]
-    assert tokens == SIMPLETOK * 10000
+    assert tokens == SIMPLETOK * 100
 
 
 def test_new_parser_eof():
-    parser = PSBaseParser(BufferedReader(BytesIO(SIMPLE1[:-1])))
+    # Make sure we get a keyword at eof
+    parser = PSBaseParser(BytesIO(SIMPLE1[:-1]))
     tokens = [tok for pos, tok in list(parser)]
     assert tokens == SIMPLETOK
 
@@ -111,23 +113,23 @@ PAGE17 = b"""
 
 
 def test_new_parser1():
-    parser = PSBaseParser(BufferedReader(BytesIO(b"123.456")))
+    parser = PSBaseParser(BytesIO(b"123.456"))
     assert list(parser) == [(0, 123.456)]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"+.013")))
+    parser = PSBaseParser(BytesIO(b"+.013"))
     assert list(parser) == [(0, 0.013)]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"123")))
+    parser = PSBaseParser(BytesIO(b"123"))
     assert list(parser) == [(0, 123)]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"true false")))
+    parser = PSBaseParser(BytesIO(b"true false"))
     assert list(parser) == [(0, True), (5, False)]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"(foobie bletch)")))
+    parser = PSBaseParser(BytesIO(b"(foobie bletch)"))
     assert list(parser) == [(0, b"foobie bletch")]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"(foo")))  # Invalid string
+    parser = PSBaseParser(BytesIO(b"(foo"))  # Invalid string
     assert list(parser) == []
 
 
 def test_new_parser_names():
     # Examples from PDF 1.7 page 17
-    parser = PSBaseParser(BufferedReader(BytesIO(PAGE17)))
+    parser = PSBaseParser(BytesIO(PAGE17))
     tokens = list(parser)
     assert tokens == [
         (5, LIT("A;Name_With-Various***Characters?")),
@@ -138,11 +140,9 @@ def test_new_parser_names():
 
 def test_new_parser_strings():
     parser = PSBaseParser(
-        BufferedReader(
-            BytesIO(
-                rb"( Strings may contain balanced parentheses ( ) and "
-                rb"special characters ( * ! & } ^ % and so on ) . )"
-            )
+        BytesIO(
+            rb"( Strings may contain balanced parentheses ( ) and "
+            rb"special characters ( * ! & } ^ % and so on ) . )"
         )
     )
     assert list(parser) == [
@@ -152,51 +152,49 @@ def test_new_parser_strings():
             rb"special characters ( * ! & } ^ % and so on ) . ",
         )
     ]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"()")))
+    parser = PSBaseParser(BytesIO(b"()"))
     assert list(parser) == [(0, b"")]
     parser = PSBaseParser(
-        BufferedReader(
-            BytesIO(
-                rb"""( These \
+        BytesIO(
+            rb"""( These \
 two strings \
 are the same . )
     """
-            )
         )
     )
     assert list(parser) == [(0, b" These two strings are the same . ")]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"(foo\rbar)")))
+    parser = PSBaseParser(BytesIO(b"(foo\rbar)"))
     assert list(parser) == [(0, b"foo\nbar")]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"(foo\r)")))
+    parser = PSBaseParser(BytesIO(b"(foo\r)"))
     assert list(parser) == [(0, b"foo\n")]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"(foo\r\nbaz)")))
+    parser = PSBaseParser(BytesIO(b"(foo\r\nbaz)"))
     assert list(parser) == [(0, b"foo\nbaz")]
-    parser = PSBaseParser(BufferedReader(BytesIO(b"(foo\n)")))
+    parser = PSBaseParser(BytesIO(b"(foo\n)"))
     assert list(parser) == [(0, b"foo\n")]
     parser = PSBaseParser(
-        BufferedReader(
-            BytesIO(rb"( This string contains \245two octal characters\307 . )")
-        )
+        BytesIO(rb"( This string contains \245two octal characters\307 . )")
     )
     assert list(parser) == [
         (0, b" This string contains \245two octal characters\307 . ")
     ]
-    parser = PSBaseParser(BufferedReader(BytesIO(rb"(\0053 \053 \53)")))
+    parser = PSBaseParser(BytesIO(rb"(\0053 \053 \53)"))
     assert list(parser) == [(0, b"\0053 \053 +")]
-    parser = PSBaseParser(
-        BufferedReader(BytesIO(rb"< 4E6F762073686D6F7A206B6120706F702E >"))
-    )
+    parser = PSBaseParser(BytesIO(rb"< 4E6F762073686D6F7A206B6120706F702E >"))
     assert list(parser) == [(0, b"Nov shmoz ka pop.")]
-    parser = PSBaseParser(BufferedReader(BytesIO(rb"<73 686 D6F7A2>")))
+    parser = PSBaseParser(BytesIO(rb"<73 686 D6F7A2>"))
     assert list(parser) == [(0, b"shmoz ")]
-    parser = PSBaseParser(BufferedReader(BytesIO(rb"(\400)")))
+    parser = PSBaseParser(BytesIO(rb"(\400)"))
     assert list(parser) == [(0, b"")]
 
 
 def test_invalid_strings_eof():
-    parser = PSBaseParser(BufferedReader(BytesIO(rb"(\00")))
+    parser = PSBaseParser(BytesIO(rb"(\00"))
     assert list(parser) == []
-    parser = PSBaseParser(BufferedReader(BytesIO(rb"(abracadab")))
+    parser = PSBaseParser(BytesIO(rb"(abracadab"))
     assert list(parser) == []
-    parser = PSBaseParser(BufferedReader(BytesIO(rb"<73686")))
+    parser = PSBaseParser(BytesIO(rb"<73686"))
     assert list(parser) == []
+
+
+def test_stack_parser():
+    pass
