63710676f329d9afe45e9aebc7df284ea607eb5e
David Huggins-Daines <dhd@ecolingui.ca>
2024-09-30 21:32:08 -0400

    fix(test): oh right its pag 2

--- tests/test_pdfstructtree.py
+++ tests/test_pdfstructtree.py
@@ -68,7 +68,7 @@ class TestClass(unittest.TestCase):
             assert 1 in pages
             assert 2 in pages
 
-            page = next(pdf.get_pages())
+            page = list(pdf.get_pages())[1]
             stree = PDFStructTree(pdf, page)
             sect = next(stree.find_all("Sect"))
             mcids = list(sect.all_mcids())
@@ -76,9 +76,5 @@ class TestClass(unittest.TestCase):
             assert None in pages
             assert 1 not in pages
             assert 2 not in pages
-            # Assure that we get the MCIDs for a content element
             for p in sect.find_all("P"):
-                # p.mcids is the MCIDs for just this element,
-                # all_mcids includes children (unsure why this is
-                # different in pdfplumber?)
-                assert set(mcid for page, mcid in p.all_mcids()).issuperset(p.mcids)
+                assert set(mcid for page, mcid in p.all_mcids()) == set(p.mcids)
