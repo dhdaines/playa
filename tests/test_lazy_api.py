7bdb9e105fee494357d5a6ad33b41bfedf7afbea
David Huggins-Daines <dhd@ecolingui.ca>
2024-11-19 10:13:08 -0500

    fix(test): remember contentobjects are lazy and thus ephemeral

--- tests/test_lazy_api.py
+++ tests/test_lazy_api.py
@@ -12,11 +12,7 @@ TESTDIR = Path(__file__).parent.parent / "samples"
 def test_content_objects():
     with playa.open(TESTDIR / "2023-06-20-PV.pdf") as pdf:
         page = pdf.pages[0]
-        objects_by_type = {}
-        for obj in page.objects:
-            objects_by_type.setdefault(obj.object_type, []).append(obj)
-        assert len(objects_by_type["image"]) == 1
-        img = objects_by_type["image"][0]
+        img = next(obj for obj in page.objects if obj.object_type == "image")
         assert tuple(img.colorspace[0]) == ("ICCBased", 3)
         ibbox = [round(x) for x in img.bbox]
         assert ibbox == [254, 899, 358, 973]
