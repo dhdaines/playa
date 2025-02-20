06e42f30c64bd74b268fa1708363d45aaccef2d8
David Huggins-Daines <dhd@ecolingui.ca>
2025-02-20 09:04:00 -0500

    feat: add extract_text benchmark

--- benchmarks/text.py
+++ benchmarks/text.py
@@ -26,15 +26,34 @@ def benchmark_chars(path: Path):
                     _ = obj.chars
 
 
+def benchmark_text(path: Path):
+    """Extract text, sort of."""
+    import playa
+
+    if path.name in PDFMINER_BUGS or path.name in XFAILS:
+        return
+    passwords = PASSWORDS.get(path.name, [""])
+    for password in passwords:
+        LOG.info("Reading %s", path)
+        with playa.open(path, password=password) as pdf:
+            for page in pdf.pages:
+                page.extract_text()
+
+
 if __name__ == "__main__":
     # Silence warnings about broken PDFs
     logging.basicConfig(level=logging.ERROR)
     niter = 5
-    miner_time = beach_time = lazy_time = 0.0
+    chars_time = text_time = 0.0
     for iter in range(niter + 1):
         for path in BASEPDFS:
             start = time.time()
             benchmark_chars(path)
             if iter != 0:
-                lazy_time += time.time() - start
-    print("chars took %.2fs / iter" % (lazy_time / niter,))
+                chars_time += time.time() - start
+            start = time.time()
+            benchmark_text(path)
+            if iter != 0:
+                text_time += time.time() - start
+    print("chars took %.2fs / iter" % (chars_time / niter,))
+    print("extract_text took %.2fs / iter" % (text_time / niter,))
