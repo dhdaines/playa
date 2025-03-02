b7b3982f26ddc7a7bd9c278d0717edbbba8aee37
David Huggins-Daines <dhd@ecolingui.ca>
2025-03-01 22:16:00 -0500

    fix: do not crash on missing unicode map (but need to update unicode maps?)

--- playa/cmapdb.py
+++ playa/cmapdb.py
@@ -158,6 +158,9 @@ class UnicodeMap(CMapBase):
         return "<UnicodeMap: %s>" % self.attrs.get("CMapName")
 
     def get_unichr(self, cid: int) -> str:
+        if cid not in self.cid2unichr:
+            log.warning("CID %d not found in Unicode map %s", cid, self)
+            return ""
         return self.cid2unichr[cid]
 
     def dump(self, out: TextIO = sys.stdout) -> None:
