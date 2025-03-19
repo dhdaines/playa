7f69ae742efb81b6d90eb0c4a0cf9ec03ef1e8ad
David Huggins-Daines <dhd@ecolingui.ca>
2025-03-19 11:49:31 -0400

    feat: possibly generate all schemas (do not do this yet though)

--- tools/create_json_schema.py
+++ tools/create_json_schema.py
@@ -1,15 +1,23 @@
 """(Re-)create the JSON schemas for playa.data classes."""
 
 import json
+from pathlib import Path
 
 from pydantic import TypeAdapter
+from typing_extensions import is_typeddict
 
 import playa.data
 
+SCHEMA = Path(playa.data.__file__).parent / "schema"
+
 
 def main():
-    doc_adapter = TypeAdapter(playa.data.Document)
-    print(json.dumps(doc_adapter.json_schema(), indent=2, ensure_ascii=False))
+    SCHEMA.mkdir(parents=True, exist_ok=True)
+    for name, obj in vars(playa.data).items():
+        if is_typeddict(obj):
+            adapter = TypeAdapter(obj)
+            with open(SCHEMA / f"{name}.json", "wt") as outfh:
+                json.dump(adapter.json_schema(), outfh, indent=2, ensure_ascii=False)
 
 
 if __name__ == "__main__":
