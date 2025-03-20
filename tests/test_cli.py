4645df79d728086c8b36f28f81284a4f1b908b95
David Huggins-Daines <dhd@ecolingui.ca>
2025-03-20 19:38:52 -0400

    tests: test structure and outlines in cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -20,6 +20,40 @@ def test_cli_metadata(path: Path):
     for password in passwords:
         try:
             main(["--password", password, "--non-interactive", str(path)])
+            main(["--password", password, "--non-interactive", "--outline", str(path)])
+            main(
+                ["--password", password, "--non-interactive", "--structure", str(path)]
+            )
+        except PDFPasswordIncorrect:
+            pass
+        except PDFEncryptionError:
+            pytest.skip("cryptography package not installed")
+
+
+@pytest.mark.parametrize("path", ALLPDFS, ids=str)
+def test_cli_outline(path: Path):
+    if path.name in XFAILS:
+        pytest.xfail("Intentionally corrupt file: %s" % path.name)
+    passwords = PASSWORDS.get(path.name, [""])
+    for password in passwords:
+        try:
+            main(["--password", password, "--non-interactive", "--outline", str(path)])
+        except PDFPasswordIncorrect:
+            pass
+        except PDFEncryptionError:
+            pytest.skip("cryptography package not installed")
+
+
+@pytest.mark.parametrize("path", ALLPDFS, ids=str)
+def test_cli_structure(path: Path):
+    if path.name in XFAILS:
+        pytest.xfail("Intentionally corrupt file: %s" % path.name)
+    passwords = PASSWORDS.get(path.name, [""])
+    for password in passwords:
+        try:
+            main(
+                ["--password", password, "--non-interactive", "--structure", str(path)]
+            )
         except PDFPasswordIncorrect:
             pass
         except PDFEncryptionError:
