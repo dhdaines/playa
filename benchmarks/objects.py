0ce7124d3a1d2586f2170876f5a10010f5008d35
David Huggins-Daines <dhd@ecolingui.ca>
2025-05-13 18:59:16 -0400

    Make benchmarks more useful (#111)
    * ci: remove benchmarks as they are un-useful in current form
    
    * fix: use standard fixed benchmark set (subject to change)
    
    * feat: simple version comparison script
    
    * fix: QUOTES!!
    
    * fix: types

--- benchmarks/objects.py
+++ benchmarks/objects.py
@@ -6,33 +6,32 @@ import logging
 import time
 from pathlib import Path
 
+import playa
 from playa import ContentObject, Rect
-from tests.data import BASEPDFS, PASSWORDS, PDFMINER_BUGS, XFAILS
 
 LOG = logging.getLogger("benchmark-convert")
+# Use a standard benchmark set to make version comparisons possible
+CONTRIB = Path(__file__).parent.parent / "samples" / "contrib"
+PDFS = [
+    "PSC_Station.pdf",
+    "Rgl-1314-2021-DM-Derogations-mineures.pdf",
+]
 
 
 def benchmark_one_lazy(path: Path):
     """Open one of the documents"""
-    import playa
-
-    if path.name in PDFMINER_BUGS or path.name in XFAILS:
-        return
-    passwords = PASSWORDS.get(path.name, [""])
-    for password in passwords:
-        LOG.info("Reading %s", path)
-        with playa.open(path, password=password) as pdf:
-            for page in pdf.pages:
-                obj: ContentObject
-                _: Rect
-                for obj in page.texts:
-                    _ = obj.bbox
-                for obj in page.paths:
-                    _ = obj.bbox
-                for obj in page.images:
-                    _ = obj.bbox
-                for obj in page.xobjects:
-                    _ = obj.bbox
+    with playa.open(path) as pdf:
+        for page in pdf.pages:
+            obj: ContentObject
+            _: Rect
+            for obj in page.texts:
+                _ = obj.bbox
+            for obj in page.paths:
+                _ = obj.bbox
+            for obj in page.images:
+                _ = obj.bbox
+            for obj in page.xobjects:
+                _ = obj.bbox
 
 
 if __name__ == "__main__":
@@ -41,9 +40,10 @@ if __name__ == "__main__":
     niter = 5
     lazy_time = 0.0
     for iter in range(niter + 1):
-        for path in BASEPDFS:
+        for name in PDFS:
+            path = CONTRIB / name
             start = time.time()
             benchmark_one_lazy(path)
             if iter != 0:
                 lazy_time += time.time() - start
-    print("Object types took %.2fs / iter" % (lazy_time / niter,))
+    print("Object types took %d ms / iter" % (lazy_time / niter * 1000,))
--- benchmarks/parallel.py
+++ benchmarks/parallel.py
@@ -8,41 +8,54 @@ from pathlib import Path
 import playa
 from playa.page import Page
 
+CONTRIB = Path(__file__).parent.parent / "samples" / "contrib"
 
-def process_page(page: Page) -> str:
-    return " ".join(x.chars for x in page.texts)
+
+def process_page(page: Page) -> None:
+    for obj in page:
+        _ = obj.bbox
 
 
 def benchmark_single(path: Path):
     with playa.open(path) as pdf:
-        return list(pdf.pages.map(process_page))
+        list(pdf.pages.map(process_page))
 
 
 def benchmark_multi(path: Path, ncpu: int):
     with playa.open(path, max_workers=ncpu) as pdf:
-        return list(pdf.pages.map(process_page))
+        list(pdf.pages.map(process_page))
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("-n", "--ncpu", type=int, default=4)
-    parser.add_argument("pdf", type=Path)
+    parser.add_argument("-n", "--ncpu", type=int, default=2)
+    parser.add_argument("-i", "--niter", type=int, default=5)
+    parser.add_argument(
+        "--pdf",
+        type=Path,
+        default=CONTRIB / "Rgl-1314-2021-DM-Derogations-mineures.pdf",
+    )
     args = parser.parse_args()
 
-    start = time.time()
-    benchmark_multi(args.pdf, args.ncpu)
-    multi_time = time.time() - start
+    multi_time = single_time = 0.0
+    for iter in range(args.niter + 1):
+        start = time.time()
+        benchmark_multi(args.pdf, args.ncpu)
+        if iter != 0:
+            multi_time += time.time() - start
     print(
-        "PLAYA (%d CPUs) took %.2fs"
+        "PLAYA (%d CPUs) took %d ms / iter"
         % (
             args.ncpu,
-            multi_time,
+            multi_time / args.niter * 1000,
         )
     )
 
-    start = time.time()
-    benchmark_single(args.pdf)
-    single_time = time.time() - start
-    print("PLAYA (single) took %.2fs" % (single_time,))
+    for iter in range(args.niter + 1):
+        start = time.time()
+        benchmark_single(args.pdf)
+        if iter != 0:
+            single_time += time.time() - start
+    print("PLAYA (single) took %d ms / iter" % (single_time / args.niter * 1000,))
